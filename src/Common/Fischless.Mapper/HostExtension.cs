9df4900b376ad57a5d9e1bcec7210cb82887db21
ema <mccoy39082@163.com>
2024-05-01 16:13:10 +0800

    add: some common libs

---
+++ src/Common/Fischless.Mapper/HostExtension.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Hosting;
+using System.Reflection;
+
+namespace Fischless.Mapper;
+
+public static class HostExtension
+{
+    public static IHostBuilder UseMapper(this IHostBuilder app, object hostOrAssembly = null!)
+    {
+        MapperAssemblyResolver.Register(hostOrAssembly is Assembly assembly ? assembly : hostOrAssembly?.GetType()?.Assembly);
+        return app;
+    }
+}
