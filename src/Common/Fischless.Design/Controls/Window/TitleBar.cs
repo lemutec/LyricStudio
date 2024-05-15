aa1541d3051507ac25544365c3235d228c4213a8
ema <mccoy39082@163.com>
2024-05-16 06:20:55 +0800

    prepare: TitleBar

---
+++ src/Common/Fischless.Design/Controls/Window/TitleBar.cs
@@ -0,0 +1,33 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+
+namespace Fischless.Design.Controls;
+
+[Obsolete("Not supported nowaday")]
+public class TitleBar : TemplatedControl
+{
+    public static readonly AvaloniaProperty IconProperty = AvaloniaProperty.Register<TitleBar, IconElement>("Icon", null);
+
+    public IconElement? Icon
+    {
+        get => (IconElement)GetValue(IconProperty);
+        set => SetValue(IconProperty, value);
+    }
+
+    public static readonly AvaloniaProperty TitleProperty = AvaloniaProperty.Register<TitleBar, string>("Title", null);
+
+    public string Title
+    {
+        get => (string)GetValue(TitleProperty);
+        set => SetValue(TitleProperty, value);
+    }
+
+    public static readonly AvaloniaProperty HeaderProperty = AvaloniaProperty.Register<TitleBar, object>("Header", null);
+
+    public object Header
+    {
+        get => GetValue(HeaderProperty);
+        set => SetValue(HeaderProperty, value);
+    }
+}
---
+++ src/Common/Fischless.Win32/Helpers/SystemMetricsHelper.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Vanara.PInvoke;
+
+namespace Fischless.Win32;
+
+public static class SystemMetricsHelper
+{
+    [SuppressMessage("Interoperability", "CA1416:Validate platform compatibility")]
+    public static (int, int) GetSystemMetrics(User32.SystemMetric metric)
+    {
+        if (Environment.OSVersion.Platform == PlatformID.Win32NT)
+        {
+            int cxMinBtn = User32.GetSystemMetrics(User32.SystemMetric.SM_CXMIN);
+            int cyMinBtn = User32.GetSystemMetrics(User32.SystemMetric.SM_CYMIN);
+            return (cxMinBtn, cyMinBtn);
+        }
+        return (default, default);
+    }
+}
