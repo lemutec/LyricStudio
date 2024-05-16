d3cbfa0226f863646e73d6a1b161157fb84faf86
ema <mccoy39082@163.com>
2024-05-16 10:16:32 +0800

    add: Card and CardAction

---
+++ src/Common/Fischless.Design/Controls/Card/Card.cs
@@ -0,0 +1,21 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Metadata;
+using Avalonia.Controls.Presenters;
+using Avalonia.Metadata;
+
+namespace Fischless.Design.Controls;
+
+[TemplatePart("PART_ContentPresenter", typeof(ContentPresenter))]
+public class Card : ContentControl
+{
+    public new static readonly StyledProperty<object?> ContentProperty = AvaloniaProperty.Register<Card, object?>("Content");
+
+    [Content]
+    [DependsOn("ContentTemplate")]
+    public new object? Content
+    {
+        get => GetValue(ContentProperty);
+        set => SetValue(ContentProperty, value);
+    }
+}
---
+++ src/Common/Fischless.Design/Controls/Card/CardAction.cs
@@ -0,0 +1,21 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Metadata;
+using Avalonia.Controls.Presenters;
+using Avalonia.Metadata;
+
+namespace Fischless.Design.Controls;
+
+[TemplatePart("PART_ContentPresenter", typeof(ContentPresenter))]
+public class CardAction : ContentControl
+{
+    public new static readonly StyledProperty<object?> ContentProperty = AvaloniaProperty.Register<CardAction, object?>("Content");
+
+    [Content]
+    [DependsOn("ContentTemplate")]
+    public new object? Content
+    {
+        get => GetValue(ContentProperty);
+        set => SetValue(ContentProperty, value);
+    }
+}
