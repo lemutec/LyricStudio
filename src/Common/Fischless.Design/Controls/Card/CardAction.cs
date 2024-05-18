648c06675b5acb78cf16849df0c8f29da389186d
ema <mccoy39082@163.com>
2024-05-19 07:59:37 +0800

    add: settings page

--- src/Common/Fischless.Design/Controls/Card/CardAction.cs
+++ src/Common/Fischless.Design/Controls/Card/CardAction.cs
@@ -2,7 +2,11 @@
 using Avalonia.Controls;
 using Avalonia.Controls.Metadata;
 using Avalonia.Controls.Presenters;
+using Avalonia.Data;
+using Avalonia.Input;
+using Avalonia.Interactivity;
 using Avalonia.Metadata;
+using System.Windows.Input;
 
 namespace Fischless.Design.Controls;
 
@@ -17,5 +21,34 @@ public class CardAction : ContentControl
     {
         get => GetValue(ContentProperty);
         set => SetValue(ContentProperty, value);
+    }
+
+    public static readonly StyledProperty<ICommand?> CommandProperty = AvaloniaProperty.Register<CardAction, ICommand>("Command", null, inherits: false, BindingMode.OneWay, null, null, enableDataValidation: true)!;
+
+    public ICommand? Command
+    {
+        get => GetValue(CommandProperty);
+        set => SetValue(CommandProperty, value);
+    }
+
+    public static readonly StyledProperty<object?> CommandParameterProperty = AvaloniaProperty.Register<CardAction, object>("CommandParameter")!;
+
+    public object? CommandParameter
+    {
+        get => GetValue(CommandParameterProperty);
+        set => SetValue(CommandParameterProperty, value);
+    }
+
+    public CardAction()
+    {
+        this.AddHandler(PointerPressedEvent, OnPointerPressed, RoutingStrategies.Tunnel);
+    }
+
+    private void OnPointerPressed(object? sender, PointerPressedEventArgs e)
+    {
+        if (Command?.CanExecute(CommandParameter) == true)
+        {
+            Command.Execute(CommandParameter);
+        }
     }
 }
--- src/Desktop/LyricStudio/ViewModels/Pages/SettingsPageViewModel.cs
+++ src/Desktop/LyricStudio/ViewModels/Pages/SettingsPageViewModel.cs
@@ -1,10 +1,102 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Fischless.Globalization;
+using Fischless.Linq;
+using FluentAvalonia.Core;
+using LyricStudio.Core.Configuration;
+using LyricStudio.Models;
+using Serilog;
+using System;
+using System.Diagnostics;
+using System.IO;
 
 namespace LyricStudio.ViewModels;
 
-public class SettingsPageViewModel : ObservableObject
+public partial class SettingsPageViewModel : ObservableObject
 {
+    [ObservableProperty]
+    private int languageIndex = (int)SmartEnum<LanguageIndex>.FromDescriptionAttribute(ConfigurationKeys.Language.Get(), Fischless.Globalization.LanguageIndex.Chinese)!;
+
+    partial void OnLanguageIndexChanged(int value)
+    {
+        string prev = SmartEnum<LanguageIndex>.ToDescriptionAttribute(SmartEnum<LanguageIndex>.FromValue(LanguageIndex));
+        string next = SmartEnum<LanguageIndex>.ToDescriptionAttribute(SmartEnum<LanguageIndex>.FromValue(value));
+
+        _ = prev;
+        MuiLanguage.SetLanguage(next);
+        ConfigurationKeys.Language.Set(next);
+        Config.Configer?.Save(AppConfig.SettingsFile);
+    }
+
     public SettingsPageViewModel()
     {
+    }
+
+    [RelayCommand]
+    private void CheckUpdate()
+    {
+        try
+        {
+            Process.Start(new ProcessStartInfo()
+            {
+                FileName = "https://github.com/lemutec/LyricStudio/releases",
+                UseShellExecute = true,
+            });
+        }
+        catch (Exception e)
+        {
+            Log.Error(e.Message);
+        }
+    }
+
+    [RelayCommand]
+    private void CheckLicense()
+    {
+        try
+        {
+            Process.Start(new ProcessStartInfo()
+            {
+                FileName = "https://github.com/lemutec/LyricStudio/blob/master/LICENSE",
+                UseShellExecute = true,
+            });
+        }
+        catch (Exception e)
+        {
+            Log.Error(e.Message);
+        }
+    }
+
+    [RelayCommand]
+    private void OpenSpecialFolder()
+    {
+        try
+        {
+            Process.Start(new ProcessStartInfo()
+            {
+                FileName = $"file://{new FileInfo(AppConfig.SettingsFile).DirectoryName}/",
+                UseShellExecute = true,
+            });
+        }
+        catch (Exception e)
+        {
+            Log.Error(e.Message);
+        }
+    }
+
+    [RelayCommand]
+    private void OpenLogsFolder()
+    {
+        try
+        {
+            Process.Start(new ProcessStartInfo()
+            {
+                FileName = $"file://{AppConfig.LogFolder}/",
+                UseShellExecute = true,
+            });
+        }
+        catch (Exception e)
+        {
+            Log.Error(e.Message);
+        }
     }
 }
