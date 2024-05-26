dae153f70b61acb6fb705d8e7c11e0c4659b6bbe
ema <mccoy39082@163.com>
2024-05-26 17:08:15 +0800

    add: use MicaSetup

---
+++ build/MicaSetup/Program.un.cs
@@ -0,0 +1,75 @@
+﻿using MicaSetup.Design.Controls;
+using MicaSetup.Services;
+using MicaSetup.Views;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: Guid("00000000-0000-0000-0000-000000000000")]
+[assembly: AssemblyTitle("LyricStudio Uninst")]
+[assembly: AssemblyProduct("LyricStudio")]
+[assembly: AssemblyDescription("LyricStudio Uninst")]
+[assembly: AssemblyCompany("Lemutec")]
+[assembly: AssemblyCopyright("Under MIT License. Copyright (c) Lemutec Contributors.")]
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
+
+namespace MicaSetup;
+
+internal class Program
+{
+    [STAThread]
+    internal static void Main()
+    {
+        Hosting.CreateBuilder()
+            .UseAsUninst()
+            .UseLogger(false)
+            .UseSingleInstance("MicaSetup")
+            .UseTempPathFork()
+            .UseElevated()
+            .UseDpiAware()
+            .UseOptions(option =>
+            {
+                option.IsCreateDesktopShortcut = true;
+                option.IsCreateUninst = true;
+                option.IsCreateRegistryKeys = true;
+                option.IsCreateStartMenu = true;
+                option.IsCreateQuickLaunch = false;
+                option.IsCreateAsAutoRun = false;
+                option.IsUseRegistryPreferX86 = null!;
+                option.IsAllowFirewall = true;
+                option.IsRefreshExplorer = true;
+                option.IsInstallCertificate = false;
+                option.ExeName = "LyricStudio.exe";
+                option.KeyName = "LyricStudio";
+                option.DisplayName = "LyricStudio";
+                option.DisplayIcon = "LyricStudio.exe";
+                option.DisplayVersion = "2.0.1.0";
+                option.Publisher = "Lemutec";
+                option.AppName = "LyricStudio";
+                option.SetupName = $"LyricStudio {Mui("UninstallProgram")}";
+                option.MessageOfPage1 = "LyricStudio";
+                option.MessageOfPage2 = Mui("ProgressTipsUninstalling");
+                option.MessageOfPage3 = Mui("UninstallFinishTips");
+            })
+            .UseServices(service =>
+            {
+                service.AddSingleton<IMuiLanguageService, MuiLanguageService>();
+                service.AddScoped<IExplorerService, ExplorerService>();
+            })
+            .CreateApp()
+            .UseMuiLanguage()
+            .UseTheme(WindowsTheme.Auto)
+            .UsePages(page =>
+            {
+                page.Add(nameof(MainPage), typeof(MainPage));
+                page.Add(nameof(UninstallPage), typeof(UninstallPage));
+                page.Add(nameof(FinishPage), typeof(FinishPage));
+            })
+            .UseDispatcherUnhandledExceptionCatched()
+            .UseDomainUnhandledExceptionCatched()
+            .UseUnobservedTaskExceptionCatched()
+            .RunApp();
+    }
+}
