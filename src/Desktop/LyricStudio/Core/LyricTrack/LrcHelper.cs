d100f8d665208e7f43fafd2094a3f9e3eadca8e4
ema <mccoy39082@163.com>
2024-06-12 01:59:44 +0800

    fix: lyric support extensions

--- src/Desktop/LyricStudio/Core/LyricTrack/LrcHelper.cs
+++ src/Desktop/LyricStudio/Core/LyricTrack/LrcHelper.cs
@@ -11,7 +11,7 @@ namespace LyricStudio.Core.LyricTrack;
 
 public static partial class LrcHelper
 {
-    public static HashSet<string> LyricExtensions { get; } = [".lrc", ".txt", ".ass"];
+    public static HashSet<string> LyricExtensions { get; } = [".lrc", ".krc", ".txt", ".ass"];
 
     /// <summary>
     /// Clear all Time Mark [00:00.000]
